eb3e9c0e872cf19262be7621aa1e6d942f5a0f6f
Dan Schulte <dan.schulte.qub@live.com>
2017-09-14 19:01:34 -0700

    Add a few more tests for Random

--- src/test/java/qub/FixedRandomTests.java
+++ src/test/java/qub/FixedRandomTests.java
@@ -19,11 +19,17 @@ public class FixedRandomTests
     @Test
     public void getRandomIntegerBetween()
     {
-        final FixedRandom random = new FixedRandom(0);
-        assertEquals(0, random.getRandomIntegerBetween(0, 9));
-        assertEquals(1, random.getRandomIntegerBetween(1, 3));
-        assertEquals(11, random.getRandomIntegerBetween(11, 11));
+        final FixedRandom r0 = new FixedRandom(0);
+        assertEquals(0, r0.getRandomIntegerBetween(0, 9));
+        assertEquals(1, r0.getRandomIntegerBetween(1, 3));
+        assertEquals(11, r0.getRandomIntegerBetween(11, 11));
+        assertEquals(7, r0.getRandomIntegerBetween(10, 7));
 
-        assertEquals(7, random.getRandomIntegerBetween(10, 7));
+        final FixedRandom r10 = new FixedRandom(10);
+        assertEquals(0, r0.getRandomIntegerBetween(0, 9));
+        assertEquals(1, r0.getRandomIntegerBetween(1, 10));
+        assertEquals(1, r0.getRandomIntegerBetween(1, 3));
+        assertEquals(11, r0.getRandomIntegerBetween(11, 11));
+        assertEquals(7, r0.getRandomIntegerBetween(10, 7));
     }
 }
