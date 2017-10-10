f868cd819660575f95688b2d7d97119d3c26a160
Dan Schulte <dan.schulte.qub@live.com>
2017-10-10 16:43:58 -0700

    Add SpinMutex and tests

---
+++ src/main/java/qub/SpinMutex.java
@@ -0,0 +1,59 @@
+package qub;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * A mutex/lock that can be used to synchronize access to shared resources between multiple threads.
+ * This lock uses an atomic variable to synchronize access to shared resources, and as such doesn't
+ * require OS interaction.
+ */
+public class SpinMutex
+{
+    private final AtomicBoolean acquired;
+
+    /**
+     * Create a new SpinMutex that is ready to be acquired.
+     */
+    public SpinMutex()
+    {
+        acquired = new AtomicBoolean(false);
+    }
+
+    /**
+     * Get whether or not this SpinMutex is currently acquired.
+     * @return Whether or not this SpinMutex is currently acquired.
+     */
+    boolean isAcquired()
+    {
+        return acquired.get();
+    }
+
+    /**
+     * Acquire this mutex. If the mutex is already acquired, this thread will block until the owning
+     * thread releases this mutex and this thread acquires the mutex.
+     */
+    public void acquire()
+    {
+        while (!tryAcquire())
+        {
+        }
+    }
+
+    /**
+     * Attempt to acquire this SpinMutex and return whether or not it was acquired.
+     * @return Whether or not the SpinMutex was acquired.
+     */
+    public boolean tryAcquire()
+    {
+        return acquired.compareAndSet(false, true);
+    }
+
+    /**
+     * Release this SpinMutex so that other threads can acquire it.
+     * @return Whether or not this SpinMutex was released.
+     */
+    public boolean release()
+    {
+        return acquired.compareAndSet(true, false);
+    }
+}
