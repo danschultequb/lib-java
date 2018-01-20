057adc3e768913b23702286c7f5610ed8c041078
Dan Schulte <dan.schulte.qub@live.com>
2018-01-20 10:32:33 -0800

    Convert InstanceOfIterator tests

--- src/test/java/qub/InstanceOfIteratorTests.java
+++ src/test/java/qub/InstanceOfIteratorTests.java
@@ -1,12 +1,24 @@
-//package qub;
-//
-//public class InstanceOfIteratorTests extends IteratorTests
-//{
-//    @Override
-//    protected Iterator<Integer> createIterator(int count, boolean started)
-//    {
-//        return new ArrayIteratorTests()
-//                .createIterator(count, started)
-//                .instanceOf(Integer.class);
-//    }
-//}
+package qub;
+
+public class InstanceOfIteratorTests
+{
+    public static void test(final TestRunner runner)
+    {
+        runner.testGroup("InstanceOfIterator<T>", new Action0()
+        {
+            @Override
+            public void run()
+            {
+                IteratorTests.test(runner, new Function2<Integer, Boolean, Iterator<Integer>>()
+                {
+                    @Override
+                    public Iterator<Integer> run(Integer count, Boolean started)
+                    {
+                        return ArrayIteratorTests.createIterator(count, started)
+                            .instanceOf(Integer.class);
+                    }
+                });
+            }
+        });
+    }
+}
