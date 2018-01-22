f4755818e7fe333859e6a1bfe99cf4959ff9ac53
Dan Schulte <dan.schulte.qub@live.com>
2018-01-22 08:06:14 -0800

    Convert Math tests

--- src/test/java/qub/MathTests.java
+++ src/test/java/qub/MathTests.java
@@ -1,106 +1,135 @@
 package qub;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class MathTests
 {
-    @Test
-    public void constructor()
+    public static void test(final TestRunner runner)
     {
-        new Math();
-    }
+        runner.testGroup("Math", new Action0()
+        {
+            @Override
+            public void run()
+            {
+                runner.test("constructor", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        new Math();
+                    }
+                });
+                
+                runner.test("minimum()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertEqual(-10, Math.minimum(-10, -5));
+                        test.assertEqual(-10, Math.minimum(-5, -10));
 
-    @Test
-    public void minimum()
-    {
-        assertEquals(-10, Math.minimum(-10, -5));
-        assertEquals(-10, Math.minimum(-5, -10));
+                        test.assertEqual(-3, Math.minimum(-3, -3));
 
-        assertEquals(-3, Math.minimum(-3, -3));
+                        test.assertEqual(-1, Math.minimum(-1, 0));
+                        test.assertEqual(-1, Math.minimum(0, -1));
+                    }
+                });
+                
+                runner.test("maximum()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertEqual(-5, Math.maximum(-10, -5));
+                        test.assertEqual(-5, Math.maximum(-5, -10));
 
-        assertEquals(-1, Math.minimum(-1, 0));
-        assertEquals(-1, Math.minimum(0, -1));
-    }
+                        test.assertEqual(-3, Math.maximum(-3, -3));
 
-    @Test
-    public void maximum()
-    {
-        assertEquals(-5, Math.maximum(-10, -5));
-        assertEquals(-5, Math.maximum(-5, -10));
+                        test.assertEqual(0, Math.maximum(-1, 0));
+                        test.assertEqual(0, Math.maximum(0, -1));
+                    }
+                });
+                
+                runner.test("isOdd()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertTrue(Math.isOdd(1));
+                        test.assertTrue(Math.isOdd(3));
+                        test.assertTrue(Math.isOdd(-1));
 
-        assertEquals(-3, Math.maximum(-3, -3));
+                        test.assertFalse(Math.isOdd(0));
+                        test.assertFalse(Math.isOdd(2));
+                        test.assertFalse(Math.isOdd(-2));
 
-        assertEquals(0, Math.maximum(-1, 0));
-        assertEquals(0, Math.maximum(0, -1));
-    }
+                        test.assertFalse(Math.isOdd.run(null));
 
-    @Test
-    public void isOdd() {
-        assertTrue(Math.isOdd(1));
-        assertTrue(Math.isOdd(3));
-        assertTrue(Math.isOdd(-1));
+                        test.assertTrue(Math.isOdd.run(1));
+                        test.assertTrue(Math.isOdd.run(3));
+                        test.assertTrue(Math.isOdd.run(-1));
 
-        assertFalse(Math.isOdd(0));
-        assertFalse(Math.isOdd(2));
-        assertFalse(Math.isOdd(-2));
+                        test.assertFalse(Math.isOdd.run(0));
+                        test.assertFalse(Math.isOdd.run(2));
+                        test.assertFalse(Math.isOdd.run(-2));
+                    }
+                });
 
-        assertFalse(Math.isOdd.run(null));
+                runner.test("isEven()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertFalse(Math.isEven(1));
+                        test.assertFalse(Math.isEven(3));
+                        test.assertFalse(Math.isEven(-1));
 
-        assertTrue(Math.isOdd.run(1));
-        assertTrue(Math.isOdd.run(3));
-        assertTrue(Math.isOdd.run(-1));
+                        test.assertTrue(Math.isEven(0));
+                        test.assertTrue(Math.isEven(2));
+                        test.assertTrue(Math.isEven(-2));
 
-        assertFalse(Math.isOdd.run(0));
-        assertFalse(Math.isOdd.run(2));
-        assertFalse(Math.isOdd.run(-2));
-    }
+                        test.assertFalse(Math.isEven.run(null));
 
-    @Test
-    public void isEven() {
-        assertFalse(Math.isEven(1));
-        assertFalse(Math.isEven(3));
-        assertFalse(Math.isEven(-1));
+                        test.assertFalse(Math.isEven.run(1));
+                        test.assertFalse(Math.isEven.run(3));
+                        test.assertFalse(Math.isEven.run(-1));
 
-        assertTrue(Math.isEven(0));
-        assertTrue(Math.isEven(2));
-        assertTrue(Math.isEven(-2));
+                        test.assertTrue(Math.isEven.run(0));
+                        test.assertTrue(Math.isEven.run(2));
+                        test.assertTrue(Math.isEven.run(-2));
+                    }
+                });
 
-        assertFalse(Math.isEven.run(null));
+                runner.test("ceiling()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertEqual(0.0, Math.ceiling(0));
+                        test.assertEqual(0.0, Math.ceiling(-0));
 
-        assertFalse(Math.isEven.run(1));
-        assertFalse(Math.isEven.run(3));
-        assertFalse(Math.isEven.run(-1));
+                        test.assertEqual(1.0, Math.ceiling(0.1));
+                        test.assertEqual(-0.0, Math.ceiling(-0.1));
 
-        assertTrue(Math.isEven.run(0));
-        assertTrue(Math.isEven.run(2));
-        assertTrue(Math.isEven.run(-2));
-    }
+                        test.assertEqual(10.0, Math.ceiling(9.9));
+                        test.assertEqual(-9.0, Math.ceiling(-9.9));
+                    }
+                });
 
-    @Test
-    public void ceiling()
-    {
-        assertEquals(0, Math.ceiling(0), 0);
-        assertEquals(0, Math.ceiling(-0), 0);
+                runner.test("floor()", new Action1<Test>()
+                {
+                    @Override
+                    public void run(Test test)
+                    {
+                        test.assertEqual(0.0, Math.floor(0));
+                        test.assertEqual(0.0, Math.floor(-0));
 
-        assertEquals(1, Math.ceiling(0.1), 0);
-        assertEquals(0, Math.ceiling(-0.1), 0);
+                        test.assertEqual(0.0, Math.floor(0.1));
+                        test.assertEqual(-1.0, Math.floor(-0.1));
 
-        assertEquals(10, Math.ceiling(9.9), 0);
-        assertEquals(-9, Math.ceiling(-9.9), 0);
-    }
-
-    @Test
-    public void floor()
-    {
-        assertEquals(0, Math.floor(0), 0);
-        assertEquals(0, Math.floor(-0), 0);
-
-        assertEquals(0, Math.floor(0.1), 0);
-        assertEquals(-1, Math.floor(-0.1), 0);
-
-        assertEquals(9, Math.floor(9.9), 0);
-        assertEquals(-10, Math.floor(-9.9), 0);
+                        test.assertEqual(9.0, Math.floor(9.9));
+                        test.assertEqual(-10.0, Math.floor(-9.9));
+                    }
+                });
+            }
+        });
     }
 }
