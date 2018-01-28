de55195c0e0ce8aecad801f4217e80052fffac5d
Dan Schulte <dan.schulte.qub@live.com>
2018-01-28 07:05:26 -0800

    Convert StringIterator tests

--- src/test/java/qub/StringIteratorTests.java
+++ src/test/java/qub/StringIteratorTests.java
@@ -1,115 +1,162 @@
 package qub;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class StringIteratorTests
 {
-    @Test
-    public void constructorWithNull()
+    public static void test(final TestRunner runner)
     {
-        final StringIterator iterator = new StringIterator(null);
-        assertIterator(iterator, false, null, 0);
+        runner.testGroup("StringIterator", new Action0()
+        {
+            @Override
+            public void run()
+            {
+                runner.testGroup("constructor", new Action0()
+                {
+                    @Override
+                    public void run()
+                    {
+                        runner.test("with null", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator(null);
+                                assertIterator(test, iterator, false, null, 0);
 
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 0);
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 0);
 
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 0);
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 0);
+                            }
+                        });
+                        
+                        runner.test("with empty", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("");
+                                assertIterator(test, iterator, false, null, 0);
+
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 0);
+
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 0);
+                            }
+                        });
+                        
+                        runner.test("with non-empty", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("ab");
+                                assertIterator(test, iterator, false, null, 0);
+
+                                test.assertTrue(iterator.next());
+                                assertIterator(test, iterator, true, 'a', 0);
+
+                                test.assertTrue(iterator.next());
+                                assertIterator(test, iterator, true, 'b', 1);
+
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 2);
+
+                                test.assertFalse(iterator.next());
+                                assertIterator(test, iterator, true, null, 2);
+                            }
+                        });
+                    }
+                });
+
+                runner.testGroup("setCurrentIndex()", new Action0()
+                {
+                    @Override
+                    public void run()
+                    {
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + -10, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(-10);
+                                assertIterator(test, iterator, true, 'h', 0);
+                            }
+                        });
+
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + 0, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(0);
+                                assertIterator(test, iterator, true, 'h', 0);
+                            }
+                        });
+
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + 1, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(1);
+                                assertIterator(test, iterator, true, 'e', 1);
+                            }
+                        });
+
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + 4, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(4);
+                                assertIterator(test, iterator, true, 'o', 4);
+                            }
+                        });
+
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + 5, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(5);
+                                assertIterator(test, iterator, true, null, 5);
+                            }
+                        });
+
+                        runner.test("with " + runner.escapeAndQuote("hello") + " and " + 6, new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final StringIterator iterator = new StringIterator("hello");
+
+                                iterator.setCurrentIndex(6);
+                                assertIterator(test, iterator, true, null, 5);
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
-    @Test
-    public void constructorWithEmpty()
+    private static void assertIterator(Test test, StringIterator iterator, boolean hasStarted, Character current, int currentIndex)
     {
-        final StringIterator iterator = new StringIterator("");
-        assertIterator(iterator, false, null, 0);
-
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 0);
-
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 0);
-    }
-
-    @Test
-    public void constructorWithNonEmpty()
-    {
-        final StringIterator iterator = new StringIterator("ab");
-        assertIterator(iterator, false, null, 0);
-
-        assertTrue(iterator.next());
-        assertIterator(iterator, true, 'a', 0);
-
-        assertTrue(iterator.next());
-        assertIterator(iterator, true, 'b', 1);
-
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 2);
-
-        assertFalse(iterator.next());
-        assertIterator(iterator, true, null, 2);
-    }
-
-    @Test
-    public void setCurrentIndexWithNegativeIndex()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(-10);
-        assertIterator(iterator, true, 'h', 0);
-    }
-
-    @Test
-    public void setCurrentIndexWithZeroIndex()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(0);
-        assertIterator(iterator, true, 'h', 0);
-    }
-
-    @Test
-    public void setCurrentIndexWithPositiveIndexLessThanElementCount()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(1);
-        assertIterator(iterator, true, 'e', 1);
-    }
-
-    @Test
-    public void setCurrentIndexWithPositiveIndexEqualToOneLessThanElementCount()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(4);
-        assertIterator(iterator, true, 'o', 4);
-    }
-
-    @Test
-    public void setCurrentIndexWithPositiveIndexEqualToElementCount()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(5);
-        assertIterator(iterator, true, null, 5);
-    }
-
-    @Test
-    public void setCurrentIndexWithPositiveIndexGreaterThanElementCount()
-    {
-        final StringIterator iterator = new StringIterator("hello");
-
-        iterator.setCurrentIndex(6);
-        assertIterator(iterator, true, null, 5);
-    }
-
-    private static void assertIterator(StringIterator iterator, boolean hasStarted, Character current, int currentIndex)
-    {
-        assertEquals(hasStarted, iterator.hasStarted());
-        assertEquals(current != null, iterator.hasCurrent());
-        assertEquals(current, iterator.getCurrent());
-        assertEquals(currentIndex, iterator.getCurrentIndex());
+        test.assertEqual(hasStarted, iterator.hasStarted());
+        test.assertEqual(current != null, iterator.hasCurrent());
+        test.assertEqual(current, iterator.getCurrent());
+        test.assertEqual(currentIndex, iterator.getCurrentIndex());
     }
 }
