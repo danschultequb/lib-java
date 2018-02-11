d35d755ce5258488c04fe6b9975d5169fafda7ed
Dan Schulte <dan.schulte.qub@live.com>
2018-02-10 16:37:58 -0800

    Improve test coverage for SpinMutex

--- tests/qub/SpinMutexTests.java
+++ tests/qub/SpinMutexTests.java
@@ -89,6 +89,87 @@ public class SpinMutexTests
                         });
                     }
                 });
+
+                runner.testGroup("criticalSection(Action0)", new Action0()
+                {
+                    @Override
+                    public void run()
+                    {
+                        runner.test("with null action", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final SpinMutex mutex = new SpinMutex();
+                                mutex.criticalSection((Action0)null);
+                                test.assertFalse(mutex.isAcquired());
+                            }
+                        });
+
+                        runner.test("with non-null action", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(final Test test)
+                            {
+                                final SpinMutex mutex = new SpinMutex();
+                                final Value<Integer> value = new Value<>();
+                                mutex.criticalSection(new Action0()
+                                {
+                                    @Override
+                                    public void run()
+                                    {
+                                        test.assertTrue(mutex.isAcquired());
+                                        value.set(20);
+                                    }
+                                });
+                                test.assertFalse(mutex.isAcquired());
+                                test.assertEqual(20, value.get());
+                            }
+                        });
+                    }
+                });
+
+                runner.testGroup("criticalSection(Function0<T>)", new Action0()
+                {
+                    @Override
+                    public void run()
+                    {
+                        runner.test("with null function", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(Test test)
+                            {
+                                final SpinMutex mutex = new SpinMutex();
+                                final Integer result = mutex.criticalSection((Function0<Integer>)null);
+                                test.assertFalse(mutex.isAcquired());
+                                test.assertNull(result);
+                            }
+                        });
+
+                        runner.test("with non-null function", new Action1<Test>()
+                        {
+                            @Override
+                            public void run(final Test test)
+                            {
+                                final SpinMutex mutex = new SpinMutex();
+                                final Value<Integer> value = new Value<>();
+                                final Boolean result = mutex.criticalSection(new Function0<Boolean>()
+                                {
+                                    @Override
+                                    public Boolean run()
+                                    {
+                                        test.assertTrue(mutex.isAcquired());
+                                        value.set(20);
+                                        return true;
+                                    }
+                                });
+                                test.assertFalse(mutex.isAcquired());
+                                test.assertEqual(20, value.get());
+                                test.assertTrue(result);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
