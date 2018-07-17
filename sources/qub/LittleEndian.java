fc7f8837bac996310168b105bec2032d319156e8
Dan Schulte <dan.schulte.qub@live.com>
2018-07-17 08:21:37 -0700

    Starting to work on BigEndian ByteOrder

--- sources/qub/LittleEndian.java
+++ sources/qub/LittleEndian.java
@@ -7,8 +7,8 @@ public class LittleEndian extends ByteOrder
     {
         final byte[] result = new byte[]
         {
-            (byte)(value >>> 0 * Bytes.bitCount),
-            (byte)(value >>> 1 * Bytes.bitCount),
+            (byte)(value >>> (0 * Bytes.bitCount)),
+            (byte)(value >>> (1 * Bytes.bitCount)),
         };
 
         PostCondition.assertNotNull(result, "result");
@@ -22,10 +22,10 @@ public class LittleEndian extends ByteOrder
     {
         final byte[] result = new byte[]
         {
-            (byte)(value >>> 0 * Bytes.bitCount),
-            (byte)(value >>> 1 * Bytes.bitCount),
-            (byte)(value >>> 2 * Bytes.bitCount),
-            (byte)(value >>> 3 * Bytes.bitCount),
+            (byte)(value >>> (0 * Bytes.bitCount)),
+            (byte)(value >>> (1 * Bytes.bitCount)),
+            (byte)(value >>> (2 * Bytes.bitCount)),
+            (byte)(value >>> (3 * Bytes.bitCount)),
         };
 
         PostCondition.assertNotNull(result, "result");
@@ -39,14 +39,14 @@ public class LittleEndian extends ByteOrder
     {
         final byte[] result = new byte[]
         {
-            (byte)(value >>> 0 * Bytes.bitCount),
-            (byte)(value >>> 1 * Bytes.bitCount),
-            (byte)(value >>> 2 * Bytes.bitCount),
-            (byte)(value >>> 3 * Bytes.bitCount),
-            (byte)(value >>> 4 * Bytes.bitCount),
-            (byte)(value >>> 5 * Bytes.bitCount),
-            (byte)(value >>> 6 * Bytes.bitCount),
-            (byte)(value >>> 7 * Bytes.bitCount),
+            (byte)(value >>> (0 * Bytes.bitCount)),
+            (byte)(value >>> (1 * Bytes.bitCount)),
+            (byte)(value >>> (2 * Bytes.bitCount)),
+            (byte)(value >>> (3 * Bytes.bitCount)),
+            (byte)(value >>> (4 * Bytes.bitCount)),
+            (byte)(value >>> (5 * Bytes.bitCount)),
+            (byte)(value >>> (6 * Bytes.bitCount)),
+            (byte)(value >>> (7 * Bytes.bitCount)),
         };
 
         PostCondition.assertNotNull(result, "result");
@@ -81,10 +81,10 @@ public class LittleEndian extends ByteOrder
         PreCondition.assertNotNull(bytes, "bytes");
         PreCondition.assertEqual(Integers.byteCount, bytes.length, "bytes.length");
 
-        return (Bytes.toUnsignedInt(bytes[0]) <<  0) |
-               (Bytes.toUnsignedInt(bytes[1]) <<  8) |
-               (Bytes.toUnsignedInt(bytes[2]) << 16) |
-               (Bytes.toUnsignedInt(bytes[3]) << 24);
+        return (Bytes.toUnsignedInt(bytes[0]) << (0 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes[1]) << (1 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes[2]) << (2 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes[3]) << (3 * Bytes.bitCount));
     }
 
     @Override
@@ -93,10 +93,10 @@ public class LittleEndian extends ByteOrder
         PreCondition.assertNotNull(bytes, "bytes");
         PreCondition.assertEqual(Integers.byteCount, bytes.getCount(), "bytes.getCount()");
 
-        return (Bytes.toUnsignedInt(bytes.get(0)) <<  0) |
-               (Bytes.toUnsignedInt(bytes.get(1)) <<  8) |
-               (Bytes.toUnsignedInt(bytes.get(2)) << 16) |
-               (Bytes.toUnsignedInt(bytes.get(3)) << 24);
+        return (Bytes.toUnsignedInt(bytes.get(0)) << (0 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes.get(1)) << (1 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes.get(2)) << (2 * Bytes.bitCount)) |
+               (Bytes.toUnsignedInt(bytes.get(3)) << (3 * Bytes.bitCount));
     }
 
     @Override
@@ -105,14 +105,14 @@ public class LittleEndian extends ByteOrder
         PreCondition.assertNotNull(bytes, "bytes");
         PreCondition.assertEqual(Longs.byteCount, bytes.length, "bytes.length");
 
-        return (Bytes.toUnsignedLong(bytes[0]) <<  0) |
-               (Bytes.toUnsignedLong(bytes[1]) <<  8) |
-               (Bytes.toUnsignedLong(bytes[2]) << 16) |
-               (Bytes.toUnsignedLong(bytes[3]) << 24) |
-               (Bytes.toUnsignedLong(bytes[4]) << 32) |
-               (Bytes.toUnsignedLong(bytes[5]) << 40) |
-               (Bytes.toUnsignedLong(bytes[6]) << 48) |
-               (Bytes.toUnsignedLong(bytes[7]) << 56);
+        return (Bytes.toUnsignedLong(bytes[0]) << (0 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[1]) << (1 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[2]) << (2 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[3]) << (3 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[4]) << (4 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[5]) << (5 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[6]) << (6 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes[7]) << (7 * Bytes.bitCount));
     }
 
     @Override
@@ -121,13 +121,13 @@ public class LittleEndian extends ByteOrder
         PreCondition.assertNotNull(bytes, "bytes");
         PreCondition.assertEqual(Longs.byteCount, bytes.getCount(), "bytes.getCount()");
 
-        return (Bytes.toUnsignedLong(bytes.get(0)) <<  0) |
-               (Bytes.toUnsignedLong(bytes.get(1)) <<  8) |
-               (Bytes.toUnsignedLong(bytes.get(2)) << 16) |
-               (Bytes.toUnsignedLong(bytes.get(3)) << 24) |
-               (Bytes.toUnsignedLong(bytes.get(4)) << 32) |
-               (Bytes.toUnsignedLong(bytes.get(5)) << 40) |
-               (Bytes.toUnsignedLong(bytes.get(6)) << 48) |
-               (Bytes.toUnsignedLong(bytes.get(7)) << 56);
+        return (Bytes.toUnsignedLong(bytes.get(0)) << (0 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(1)) << (1 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(2)) << (2 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(3)) << (3 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(4)) << (4 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(5)) << (5 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(6)) << (6 * Bytes.bitCount)) |
+               (Bytes.toUnsignedLong(bytes.get(7)) << (7 * Bytes.bitCount));
    }
 }
