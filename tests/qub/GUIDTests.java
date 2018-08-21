16b4f0a23ee1819b069657f38f5f6814c6eb1e60
Dan Schulte <dan.schulte.qub@live.com>
2018-08-20 20:04:57 -0700

    Add GUID class

---
+++ tests/qub/GUIDTests.java
@@ -0,0 +1,54 @@
+package qub;
+
+public class GUIDTests
+{
+    public static void test(TestRunner runner)
+    {
+        runner.testGroup(GUID.class, () ->
+        {
+            final GUID guid = GUID.createRandom();
+
+            runner.testGroup("equals(Object)", () ->
+            {
+                runner.test("with null", (Test test) ->
+                {
+                    test.assertFalse(guid.equals((Object)null));
+                });
+
+                runner.test("with \"hello\"", (Test test) ->
+                {
+                    test.assertFalse(guid.equals((Object)"hello"));
+                });
+
+                runner.test("with same", (Test test) ->
+                {
+                    test.assertTrue(guid.equals((Object)guid));
+                });
+            });
+
+            runner.testGroup("equals(GUID)", () ->
+            {
+                runner.test("with null", (Test test) ->
+                {
+                    test.assertFalse(guid.equals((GUID)null));
+                });
+
+                runner.test("with same", (Test test) ->
+                {
+                    test.assertTrue(guid.equals(guid));
+                });
+            });
+
+            runner.test("toString()", (Test test) ->
+            {
+                final String guidString = "123e4567-e89b-12d3-a456-426655440000";
+                test.assertEqual(guidString, GUID.parseString(guidString).toString());
+            });
+
+            runner.test("createRandom()", (Test test) ->
+            {
+                test.assertNotEqual(GUID.createRandom(), GUID.createRandom());
+            });
+        });
+    }
+}
