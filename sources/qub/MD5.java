3c6ca703099b2438bdb1d6b193e8ac48188fde28
Dan Schulte <dan.schulte.qub@live.com>
2019-02-18 20:35:36 -0800

    Implement MD5 using Java's built in MessageDigest class

--- sources/qub/MD5.java
+++ sources/qub/MD5.java
@@ -1,140 +1,63 @@
 package qub;
 
+import java.security.MessageDigest;
+
 /**
  * A hash function that uses the MD5 hash algorithm to hash a sequence of bits.
  */
-public class MD5
+public interface MD5
 {
-    private final static int BlockBitCount = 512;
-    private final static int resultBitCount = 128;
-
-    private static final int S11 =  7;
-    private static final int S12 = 12;
-    private static final int S13 = 17;
-    private static final int S14 = 22;
-    private static final int S21 =  5;
-    private static final int S22 =  9;
-    private static final int S23 = 14;
-    private static final int S24 = 20;
-    private static final int S31 =  4;
-    private static final int S32 = 11;
-    private static final int S33 = 16;
-    private static final int S34 = 23;
-    private static final int S41 =  6;
-    private static final int S42 = 10;
-    private static final int S43 = 15;
-    private static final int S44 = 21;
-
-    /**
-     * Get the padding that should be added to the end of the message to hash.
-     * @param bitCount The number of bits in the message to hash.
-     * @return The bits to append to the end of the message to hash.
-     */
-    public BitArray getPaddingFromBitCount(long bitCount)
-    {
-        int resultBitCount = BlockBitCount - (int)(bitCount % BlockBitCount);
-        if (resultBitCount <= Long.SIZE)
-        {
-            resultBitCount += BlockBitCount;
-        }
-        final BitArray result = new BitArray(resultBitCount);
-        result.set(0, 1);
-
-        final long littleEndianBitCount = ByteOrder.toLittleEndianLong(bitCount);
-        result.setLastBitsFromLong(littleEndianBitCount);
-
-        PostCondition.assertNotNull(result, "result");
-        PostCondition.assertGreaterThanOrEqualTo(result.getCount(), 65, "result.getCount()");
-
-        return result;
-    }
-
-    public BitArray hash(BitArray bits)
+    static BitArray hash(BitArray bits)
     {
         PreCondition.assertNotNull(bits, "bytes");
 
-        final long bitCount = bits.getCount();
-        final BitArray padding = getPaddingFromBitCount(bitCount);
-        final BitArray paddedBits = bits.concatenate(padding);
-
-        final Value<Integer> A = new Value<>(ByteOrder.toLittleEndianInteger(0x1234567));
-        final Value<Integer> B = new Value<>(ByteOrder.toLittleEndianInteger(0x89abcdef));
-        final Value<Integer> C = new Value<>(ByteOrder.toLittleEndianInteger(0xfedcba98));
-        final Value<Integer> D = new Value<>(ByteOrder.toLittleEndianInteger(0x76543210));
-
-        int[] X = new int[BlockBitCount / Integers.bitCount];
-
-        // for (int i = 0; i < )
-
-        final BitArray result = new BitArray(resultBitCount);
+        BitArray result;
+        try
+        {
+            final java.security.MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(bits.toByteArray());
+            result = BitArray.createFromBytes(messageDigest.digest());
+        }
+        catch (Throwable error)
+        {
+            throw Exceptions.asRuntime(error);
+        }
 
         PostCondition.assertNotNull(result, "result");
-        PostCondition.assertEqual(resultBitCount, result.getCount(), "result.getCount()");
 
         return result;
     }
 
-    public BitArray hash(byte[] bytes)
+    static BitArray hash(byte[] bytes)
     {
         PreCondition.assertNotNull(bytes, "bytes");
 
-        final BitArray result = new BitArray(resultBitCount);
+        final BitArray result = hash(BitArray.createFromBytes(bytes));
 
         PostCondition.assertNotNull(result, "result");
-        PostCondition.assertEqual(resultBitCount, result.getCount(), "result.getCount()");
 
         return result;
     }
 
-    public BitArray hash(Iterator<Byte> bytes)
+    static Result<BitArray> hash(Iterator<Byte> bytes)
     {
         PreCondition.assertNotNull(bytes, "bytes");
 
-        long bitsRead = 0;
-
-        int A = 0x01234567;
-        int B = 0x89abcdef;
-        int C = 0xfedcba98;
-        int D = 0x76543210;
-
-        final BitArray result = new BitArray(resultBitCount);
+        final Result<BitArray> result = hash(bytes.toList());
 
         PostCondition.assertNotNull(result, "result");
-        PostCondition.assertEqual(resultBitCount, result.getCount(), "result.getCount()");
 
         return result;
     }
 
-    int F(int X, int Y, int Z)
-    {
-        return (X & Y) | (~X & Z);
-    }
-
-    int G(int X, int Y, int Z)
-    {
-        return (X & Z) | (Y & ~Z);
-    }
-
-    int H(int X, int Y, int Z)
-    {
-        return X ^ Y ^ Z;
-    }
-
-    int I(int X, int Y, int Z)
-    {
-        return Y ^ (X | ~Z);
-    }
-
-    // void FF(Value<Integer> a, Value<Integer> b, Value<Integer> c, Value<Integer> d, int k, int s, int i)
-
-    public BitArray hash(Iterable<Byte> bytes)
+    static Result<BitArray> hash(Iterable<Byte> bytes)
     {
         PreCondition.assertNotNull(bytes, "bytes");
 
-        final BitArray result = new BitArray(resultBitCount);
+        final Result<BitArray> result = Array.toByteArray(bytes)
+            .then((byte[] byteArray) -> MD5.hash(byteArray));
 
         PostCondition.assertNotNull(result, "result");
-        PostCondition.assertEqual(resultBitCount, result.getCount(), "result.getCount()");
 
         return result;
     }
--- tests/qub/MD5Tests.java
+++ tests/qub/MD5Tests.java
@@ -4,150 +4,18 @@ public class MD5Tests
 {
     public static void test(TestRunner runner)
     {
-        final MD5 md5 = new MD5();
-
         runner.testGroup(MD5.class, () ->
         {
-            runner.testGroup("getPaddingFromBitCount(long)", () ->
+            final Action2<String,Function1<String,Result<BitArray>>> hashTestGroup = (String hashFunctionSignature, Function1<String,Result<BitArray>> hashCall) ->
             {
-                runner.test("with 0", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(512);
-                    expectedPadding.set(0, 1);
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(0));
-                });
-
-                runner.test("with 1", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(511);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(1));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(1));
-                });
-
-                runner.test("with 2", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(510);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(2));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(2));
-                });
-
-                runner.test("with 3", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(509);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(3));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(3));
-                });
-
-                runner.test("with 4", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(508);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(4));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(4));
-                });
-
-                runner.test("with 445", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(67);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(445));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(445));
-                });
-
-                runner.test("with 446", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(66);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(446));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(446));
-                });
-
-                runner.test("with 447", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(65);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(447));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(447));
-                });
-
-                runner.test("with 448", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(576);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(448));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(448));
-                });
-
-                runner.test("with 449", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(575);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(449));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(449));
-                });
-
-                runner.test("with 450", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(574);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(450));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(450));
-                });
-
-                runner.test("with 451", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(573);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(451));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(451));
-                });
-
-                runner.test("with 510", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(514);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(510));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(510));
-                });
-
-                runner.test("with 511", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(513);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(511));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(511));
-                });
-
-                runner.test("with 512", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(512);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(512));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(512));
-                });
-
-                runner.test("with 513", (Test test) ->
-                {
-                    final BitArray expectedPadding = new BitArray(511);
-                    expectedPadding.set(0, 1);
-                    expectedPadding.setLastBitsFromLong(ByteOrder.toLittleEndianLong(513));
-                    test.assertEqual(expectedPadding, md5.getPaddingFromBitCount(513));
-                });
-            });
-
-            final Action2<String,Function1<String,BitArray>> hashTestGroup = (String hashFunctionSignature, Function1<String,BitArray> hashCall) ->
-            {
-                runner.testGroup(hashFunctionSignature, runner.skip(), () ->
+                runner.testGroup(hashFunctionSignature, () ->
                 {
                     final Action2<String,String> hashTest = (String message, String expectedDigest) ->
                     {
                         runner.test("with " + Strings.escapeAndQuote(message), (Test test) ->
                         {
-                            final BitArray actualDigestBits = hashCall.run(message);
-                            test.assertEqual(expectedDigest, actualDigestBits.toHexString());
+                            final Result<BitArray> actualDigestBits = hashCall.run(message);
+                            test.assertEqual(expectedDigest, actualDigestBits.await().toHexString().toLowerCase());
                         });
                     };
 
@@ -165,25 +33,25 @@ public class MD5Tests
             {
                 final byte[] bytes = Strings.isNullOrEmpty(message) ? new byte[0] : CharacterEncoding.US_ASCII.encode(message).getValue();
                 final BitArray bits = Array.createBitArrayFromBytes(bytes);
-                return md5.hash(bits);
+                return Result.success(MD5.hash(bits));
             });
 
             hashTestGroup.run("hash(byte[])", (String message) ->
             {
                 final byte[] bytes = Strings.isNullOrEmpty(message) ? new byte[0] : CharacterEncoding.US_ASCII.encode(message).getValue();
-                return md5.hash(bytes);
+                return Result.success(MD5.hash(bytes));
             });
 
             hashTestGroup.run("hash(Iterator<Byte>)", (String message) ->
             {
                 final Iterator<Byte> bytes = Iterator.createFromBytes(Strings.isNullOrEmpty(message) ? new byte[0] : CharacterEncoding.US_ASCII.encode(message).await());
-                return md5.hash(bytes);
+                return MD5.hash(bytes);
             });
 
             hashTestGroup.run("hash(Iterable<Byte>)", (String message) ->
             {
                 final Iterable<Byte> bytes = Array.createByte(Strings.isNullOrEmpty(message) ? new byte[0] : CharacterEncoding.US_ASCII.encode(message).await());
-                return md5.hash(bytes);
+                return MD5.hash(bytes);
             });
         });
     }
