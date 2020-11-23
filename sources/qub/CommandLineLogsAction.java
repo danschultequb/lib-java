dc77272948b2e613f6c495c217a6750e87df85ad
Dan Schulte <dan.schulte.qub@live.com>
2020-11-22 16:18:02 -0800

    Add CommandLineLogAction.addLogStreams(Folder,Iterable<CharacterWriteStream>) function

--- sources/qub/CommandLineLogsAction.java
+++ sources/qub/CommandLineLogsAction.java
@@ -17,21 +17,73 @@ public interface CommandLineLogsAction
             .setDescription(CommandLineLogsAction.actionDescription);
     }
 
+    static Folder getQubProjectLogsFolder(Folder projectDataFolder)
+    {
+        PreCondition.assertNotNull(projectDataFolder, "projectDataFolder");
+
+        return projectDataFolder.getFolder("logs").await();
+    }
+
     static void run(QubProcess process)
     {
         PreCondition.assertNotNull(process, "process");
 
-        final Folder qubBuildDataFolder = process.getQubProjectDataFolder().await();
-        final Folder qubProjectLogsFolder = qubBuildDataFolder.getFolder("logs").await();
-        if (!qubProjectLogsFolder.exists().await())
+        final Folder projectDataFolder = process.getQubProjectDataFolder().await();
+        final Folder projectLogsFolder = CommandLineLogsAction.getQubProjectLogsFolder(projectDataFolder);
+        if (!projectLogsFolder.exists().await())
         {
-            process.getOutputWriteStream().writeLine("The logs folder (" + qubProjectLogsFolder + ") doesn't exist.").await();
+            process.getOutputWriteStream().writeLine("The logs folder (" + projectLogsFolder + ") doesn't exist.").await();
         }
         else
         {
-            final Path qubProjectLogsFolderPath = qubProjectLogsFolder.getPath();
+            final Path projectLogsFolderPath = projectLogsFolder.getPath();
             final DefaultApplicationLauncher applicationLauncher = process.getDefaultApplicationLauncher();
-            applicationLauncher.openFileWithDefaultApplication(qubProjectLogsFolderPath).await();
+            applicationLauncher.openFileWithDefaultApplication(projectLogsFolderPath).await();
         }
+    }
+
+    /**
+     * Combine the provided streams with a log stream so that any characters that are written to
+     * the provided streams will also be written to log stream.
+     * @param projectDataFolder The data folder for the running application/project.
+     * @param streams The streams that will be combined with the created log stream.
+     * @return The combined streams in the same order that they appeared in the provided streams Iterable.
+     */
+    static Indexable<CharacterWriteStream> addLogStreams(Folder projectDataFolder, Iterable<CharacterWriteStream> streams)
+    {
+        PreCondition.assertNotNull(projectDataFolder, "projectDataFolder");
+        PreCondition.assertNotNullAndNotEmpty(streams, "streams");
+
+
+        final Folder projectLogsFolder = CommandLineLogsAction.getQubProjectLogsFolder(projectDataFolder);
+        final int logFileCount = projectLogsFolder.getFiles()
+            .then((Iterable<File> logFiles) -> logFiles.getCount())
+            .catchError(NotFoundException.class, () -> 0)
+            .await();
+        final String logFileName = (logFileCount + 1) + ".log";
+        final CharacterWriteStream logStream = projectLogsFolder.getFile(logFileName).await()
+            .getContentsCharacterWriteStream().await();
+
+        final List<CharacterWriteStream> result = List.create();
+        for (CharacterWriteStream stream : streams)
+        {
+            if (stream instanceof VerboseCharacterWriteStream)
+            {
+                result.add(CharacterWriteStreamList.create(stream, new VerboseCharacterWriteStream(true, logStream)));
+            }
+            else if (stream != null)
+            {
+                result.add(CharacterWriteStreamList.create(stream, logStream));
+            }
+            else
+            {
+                result.add(null);
+            }
+        }
+
+        PostCondition.assertNotNullAndNotEmpty(result, "result");
+        PostCondition.assertEqual(streams.getCount(), result.getCount(), "result.getCount()");
+
+        return result;
     }
 }
